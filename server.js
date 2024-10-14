4870088dd3ae684b63cb64cf349b21c317b4b124
Ana <asya1551@gmail.com>
2024-10-13 22:24:38 -0400

    feat: add UI

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import usersRoutes from './routes/users.js'
+import usersRoutes from './routes/users.js';
+import path from 'path';
 
 const app = express();
-const PORT = 5001;
+const PORT = 5002;
+
+let staticPath = path.join(path.resolve(), 'public');
 
 app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use('/api/users', usersRoutes);
 
 // GET '/'
 app.get('/', (req, res) => {
-  console.info('GET request to endpoint "/" received.');
-
-  res.send('Node Express API Server');
+  res.sendFile(path.join(staticPath, 'index.html'));
 })
 
-app.use('/users', usersRoutes);
+app.get('/api/', (req, res) => {
+  console.info('GET request to endpoint "/" received.');
+
+  res.send('Node Express API Server App');
+})
 
 app.listen(PORT, () => console.info(`Server is running on http://localhost:${PORT}`))
